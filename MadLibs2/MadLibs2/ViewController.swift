5c7ad957437145110ab94debec52c281a5edccf5
zach lysobey <zach@lysobey.com>
2015-11-08 17:31:56 -0500

    add main view outlets and actions

--- MadLibs2/MadLibs2/ViewController.swift
+++ MadLibs2/MadLibs2/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var word1: UITextField!
+    @IBOutlet weak var word1Label: UILabel!
+    
+    @IBOutlet weak var word2: UITextField!
+    @IBOutlet weak var word2Label: UILabel!
+    
+    @IBOutlet weak var word3: UITextField!
+    @IBOutlet weak var word3Label: UILabel!
+    
+    @IBAction func editTemplate(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func generateStory(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
