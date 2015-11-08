c6eec3fadbc7c8fbbce495e71aa799e17f843091
zach lysobey <zach@lysobey.com>
2015-11-08 17:09:11 -0500

    create Xcode project

---
+++ MadLibs2/MadLibs2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MadLibs2
+//
+//  Created by zach lysobey on 11/8/15.
+//  Copyright © 2015 Abstract Evolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
