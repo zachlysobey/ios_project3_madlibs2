4c94924e4859b77b3f1b92858b95746c839f3b58
zach lysobey <zach@lysobey.com>
2015-11-18 23:08:37 -0500

    set up initial user defaults for placeholders and labels

--- MadLibs2/MadLibs2/ViewController.swift
+++ MadLibs2/MadLibs2/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // User Default Keys
+    let kWord1LabelText = "word1LabelText"
+    let kWord1Placeholder = "word1Placeholder"
+    let kWord2LabelText = "word2LabelText"
+    let kWord2Placeholder = "word2Placeholder"
+    let kWord3LabelText = "word3LabelText"
+    let kWord3Placeholder = "word3Placeholder"
+
     @IBOutlet weak var word1: UITextField!
     @IBOutlet weak var word1Label: UILabel!
     
@@ -18,16 +26,88 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var word3: UITextField!
     @IBOutlet weak var word3Label: UILabel!
+
     
     @IBAction func editTemplate(sender: AnyObject) {
-        
+        // Do I need this?
     }
-
+    
     @IBAction func generateStory(sender: AnyObject) {
         self.performSegueWithIdentifier("generateStorySegue", sender: self)
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setupUserDefaultsIfNil()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTextFieldPlaceholdersAndLabelsToUserDefaults()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        passWordChoicesToStoryOutputViewController(segue)
+    }
+    
+    func setupUserDefaultsIfNil() {
+        let defaultWord1Label = "Noun"
+        let defaultWord1Placeholder = "Your Noun"
+        let defaultWord2Label = "Adjective"
+        let defaultWord2Placeholder = "Your Adjective"
+        let defaultWord3Label = "Verb"
+        let defaultWord3Placeholder = "Your Verb"
+        
+        let userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        if userDefaults.stringForKey(kWord1LabelText) == nil {
+            userDefaults.setObject(defaultWord1Label, forKey: kWord1LabelText)
+        }
+        
+        if userDefaults.stringForKey(kWord1Placeholder) == nil {
+            userDefaults.setObject(defaultWord1Placeholder, forKey: kWord1Placeholder)
+        }
+        
+        if userDefaults.stringForKey(kWord2LabelText) == nil {
+            userDefaults.setObject(defaultWord2Label, forKey: kWord2LabelText)
+        }
+        
+        if userDefaults.stringForKey(kWord2Placeholder) == nil {
+            userDefaults.setObject(defaultWord2Placeholder, forKey: kWord2Placeholder)
+        }
+        
+        if userDefaults.stringForKey(kWord3LabelText) == nil {
+            userDefaults.setObject(defaultWord3Label, forKey: kWord3LabelText)
+        }
+        
+        if userDefaults.stringForKey(kWord3Placeholder) == nil {
+            userDefaults.setObject(defaultWord3Placeholder, forKey: kWord3Placeholder)
+        }
+//        print("what are my defaults now?")
+//        print(userDefaults.objectForKey(kWord1Placeholder) as? String)
+//        print(userDefaults.objectForKey(kWord1LabelText) as? String)
+//        print(userDefaults.objectForKey(kWord2Placeholder) as? String)
+//        print(userDefaults.objectForKey(kWord2LabelText) as? String)
+//        print(userDefaults.objectForKey(kWord3Placeholder) as? String)
+//        print(userDefaults.objectForKey(kWord3LabelText) as? String)
+    }
+    
+    func setTextFieldPlaceholdersAndLabelsToUserDefaults() {
+        let userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        word1.placeholder = userDefaults.objectForKey(kWord1Placeholder) as? String
+        word1Label.text = userDefaults.objectForKey(kWord1LabelText) as? String
+        word2.placeholder = userDefaults.objectForKey(kWord2Placeholder) as? String
+        word2Label.text = userDefaults.objectForKey(kWord2LabelText) as? String
+        word3.placeholder = userDefaults.objectForKey(kWord3Placeholder) as? String
+        word3Label.text = userDefaults.objectForKey(kWord3LabelText) as? String
+    }
+    
+    func passWordChoicesToStoryOutputViewController(segue: UIStoryboardSegue) {
         let viewController = segue.destinationViewController as! StoryOutputViewController
         viewController.words = [
             word1.text!,
@@ -35,12 +115,5 @@ class ViewController: UIViewController {
             word3.text!
         ]
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
 }
