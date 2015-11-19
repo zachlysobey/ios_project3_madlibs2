f1992f34f439927ab6d6b35e09c99dd3ab13ede1
zach lysobey <zach@lysobey.com>
2015-11-18 23:19:35 -0500

    start rudimentary story generation

--- MadLibs2/MadLibs2/StoryOutputViewController.swift
+++ MadLibs2/MadLibs2/StoryOutputViewController.swift
@@ -11,12 +11,27 @@ import UIKit
 class StoryOutputViewController: UIViewController {
 
     var words: [String] = []
+    
+    @IBOutlet weak var generatedStoryOutlet: UITextView!
+    
+    let defaultStory: String =
+        "There once was a <word1> who liked to <word3> " +
+        "<word2> programs on his computer. That <word1> " +
+        "would <word3> so much that it hurt."
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        generatedStoryOutlet.text = getStoryTemplate()
+            .stringByReplacingOccurrencesOfString("<word1>", withString: "Heyo!")
+            .stringByReplacingOccurrencesOfString("<word2>", withString: "Boom!")
+            .stringByReplacingOccurrencesOfString("<word3>", withString: "Zap!!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func getStoryTemplate() -> String {
+        return defaultStory
     }
 }
