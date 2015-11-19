2d7fff1b8dcbf7a61c1ff26ba77594cf6efb7158
zach lysobey <zach@lysobey.com>
2015-11-19 13:14:16 -0500

    wire up outlets for template editor

--- MadLibs2/MadLibs2/TemplateEditorViewController.swift
+++ MadLibs2/MadLibs2/TemplateEditorViewController.swift
@@ -10,29 +10,26 @@ import UIKit
 
 class TemplateEditorViewController: UIViewController {
     
+    @IBOutlet weak var word1LabelField: UITextField!
+    @IBOutlet weak var word1PlaceholderField: UITextField!
+    @IBOutlet weak var word2LabelField: UITextField!
+    @IBOutlet weak var word2PlaceholderField: UITextField!
+    @IBOutlet weak var word3LabelField: UITextField!
+    @IBOutlet weak var word3PlaceholderField: UITextField!
+    
+    @IBOutlet weak var storyTemplateField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
     @IBAction func doneButtonAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil);
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
